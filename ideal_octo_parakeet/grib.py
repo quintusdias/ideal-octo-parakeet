8185eaebf9c9fea32680c5f0359c2acf90d6c7a7
John G Evans <john.g.evans@noaa.gov>
2017-03-10 22:31:23 +0000

    blah

--- ideal_octo_parakeet/grib.py
+++ ideal_octo_parakeet/grib.py
@@ -1,6 +1,23 @@
+import struct
+
 class Grib(object):
     def __init__(self, inputfile):
         self.file = inputfile
+
+        self.parse()
+
+    def parse(self):
+        with open(self.file, 'rb') as f:
+
+            buffer = f.read(16)
+            indicator_section = struct.unpack('>4sHBBQ', buffer)
+            magic, reserved, discipline, edition, length = indicator_section
+
+            # assert magic == b'GRIB'
+
+            self.discipline = discipline
+            self.edition = edition
+            self.grib_message_length = length
 
     def __str__(self):
         return "blah"
